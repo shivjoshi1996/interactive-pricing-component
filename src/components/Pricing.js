a1c987a0efac9d8ff36ee7677d4bb0b86f73b68b
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-07-27 14:50:29 -0700

    Fix desktop styling, fix 1million UI bug where 1m was was not showing on pageviews section

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -11,32 +11,47 @@ const StyledPricingContainer = styled.div`
   background-color: white;
   border-radius: 8px;
   box-shadow: 0px 20px 30px -5px rgba(127, 137, 185, 0.152073);
-  padding-top: 34px;
-  padding-bottom: 32px;
+  padding-top: 2.125rem;
+  padding-bottom: 2rem;
 
   @media (min-width: 768px) {
     min-height: 100%;
-    padding-bottom: 32px;
+    padding-bottom: 2rem;
+  }
+`;
+
+const StyledSliderContainer = styled.div`
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-grid-template-rows: auto;
+    grid-template-areas:
+      'pageviews pricing'
+      'slider slider';
+    justify-items: center;
+    align-items: center;
+    row-gap: 1rem;
   }
 `;
 
 const StyledPricingInput = styled.input`
   -webkit-appearance: none;
   width: 80%;
-  border-radius: 50px;
+  border-radius: 3.125rem;
   height: 8px;
   background: #f1f5fe;
   outline: none;
   -webkit-transition: 0.2s;
   transition: opacity 0.2s;
-  margin-top: 40px;
-  margin-bottom: 40px;
+  margin-top: 2.5rem;
+  margin-bottom: 3.125rem;
+  grid-area: slider;
 
   &::-webkit-slider-thumb {
     -webkit-appearance: none;
     appearance: none;
-    width: 40px;
-    height: 40px;
+    width: 2.5rem;
+    height: 2.5rem;
     border-radius: 100px;
     background-color: #10d8c4;
     cursor: pointer;
@@ -53,39 +68,45 @@ const StyledPricingInput = styled.input`
 `;
 
 const StyledPageViews = styled.p`
-  font-size: 10px;
+  font-size: 0.625rem;
   color: #848ead;
   text-transform: uppercase;
   font-weight: 800;
-  line-height: 16px;
-  letter-spacing: 2px;
+  line-height: 1rem;
+  letter-spacing: 0.125rem;
+  grid-area: pageviews;
 
   @media (min-width: 768px) {
-    font-size: 14px;
+    font-size: 0.875rem;
   }
 `;
 
 const StyledMonthlyPrice = styled.p`
-  font-size: 14px;
+  font-size: 0.875rem;
   color: #848ead;
-  padding-bottom: 34px;
+  padding-bottom: 2.125rem;
+  grid-area: pricing;
+
+  @media (min-width: 768px) {
+    padding-bottom: 0;
+  }
 
   span {
     color: #293356;
-    font-size: 32px;
+    font-size: 2rem;
     font-weight: 800;
   }
 `;
 
 const StyledHr = styled.hr`
-  margin-top: 38px;
-  height: 1px;
+  margin-top: 2.375rem;
+  height: 0.0625rem;
   background-color: #ecf0fb;
   border: none;
 `;
 
 const StyledBottomContainer = styled.div`
-  margin-top: 24px;
+  margin-top: 1.5rem;
 
   @media (min-width: 768px) {
     display: flex;
@@ -93,7 +114,7 @@ const StyledBottomContainer = styled.div`
     justify-content: space-around;
     align-items: center;
     margin: 0 auto;
-    margin-top: 32px;
+    margin-top: 2rem;
   }
 `;
 
@@ -102,7 +123,7 @@ const StyledUnorderedList = styled.ul`
   list-style-type: circle;
   width: fit-content;
   margin: 0 auto;
-  margin-bottom: 32px;
+  margin-bottom: 2rem;
   list-style-position: inside;
 
   @media (min-width: 768px) {
@@ -113,8 +134,8 @@ const StyledUnorderedList = styled.ul`
 
 const StyledListItem = styled.li`
   color: #848ead;
-  font-size: 12px;
-  margin-bottom: 10px;
+  font-size: 0.75rem;
+  margin-bottom: 0.625rem;
 `;
 
 export default function Pricing() {
@@ -123,9 +144,10 @@ export default function Pricing() {
   const [isYearlyBilling, setIsYearlyBilling] = useState(false);
   const [billingCost, setBillingCost] = useState(24);
 
-  function handleChange(e) {
-    const { value } = e.currentTarget;
+  function handleSliderChange(e) {
+    const { value } = e.target;
     setSliderValue(value);
+    console.log(sliderValue);
   }
 
   function handleCheckBoxChange(e) {
@@ -136,7 +158,7 @@ export default function Pricing() {
     if (sliderValue < 1000000) {
       const thousand = `${sliderValue / 1000}K`;
       setPageViews(thousand);
-    } else if (sliderValue > 1000000) {
+    } else if (sliderValue >= 1000000) {
       const million = `${sliderValue / 1000000}M`;
       setPageViews(million);
     }
@@ -173,18 +195,20 @@ export default function Pricing() {
 
   return (
     <StyledPricingContainer>
-      <StyledPageViews>{pageViews} Pageviews</StyledPageViews>
-      <StyledPricingInput
-        type="range"
-        min="10000"
-        max="1000000"
-        step="10000"
-        value={sliderValue}
-        onChange={handleChange}
-      />
-      <StyledMonthlyPrice>
-        <span>${billingCost}</span> / Month
-      </StyledMonthlyPrice>
+      <StyledSliderContainer>
+        <StyledPageViews>{pageViews} Pageviews</StyledPageViews>
+        <StyledPricingInput
+          type="range"
+          min="10000"
+          max="1000000"
+          step="10000"
+          value={sliderValue}
+          onChange={handleSliderChange}
+        />
+        <StyledMonthlyPrice>
+          <span>${billingCost}</span> / Month
+        </StyledMonthlyPrice>
+      </StyledSliderContainer>
 
       <Toggle checked={isYearlyBilling} onChange={handleCheckBoxChange} />
 
